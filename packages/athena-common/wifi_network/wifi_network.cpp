a34c1ada77b5d16e291b6cd1011f0aaf8d4d3458
panweiji <406798106@qq.com>
2022-07-21 15:19:06 +0800

    chore: 嵌入式单片机程序monorepo单体式仓库

--- packages/athena-common/wifi_network/wifi_network.cpp
+++ packages/athena-common/wifi_network/wifi_network.cpp
@@ -17,6 +17,7 @@ unsigned long interval = 30000;
 /* 设置Wifi */
 // 参考文档: https://randomnerdtutorials.com/esp32-useful-wi-fi-functions-arduino/
 void init_wifi() {
+    Serial.println("开始初始化WiFi模块...");
     // ESP32 WiFiMulti功能：连接到多个网络中的最强的 Wi-Fi 网络
     // 站模式：ESP32 连接到接入点连接到另一个网络, 它必须处于工作站模式
     WiFi.mode(WIFI_STA);
