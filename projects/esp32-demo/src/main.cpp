8b05cc95340419a084931aabb722503971424c12
panweiji <406798106@qq.com>
2022-07-21 16:59:03 +0800

    feat: 无线Wifi和蓝牙模块功能

--- projects/esp32-demo/src/main.cpp
+++ projects/esp32-demo/src/main.cpp
@@ -14,7 +14,7 @@ void setup() {
     // 初始化蓝牙设置
     // init_bluetooth("ESP32-PanWeiJi");
     // 初始化Wifi无线网络
-    // init_wifi();
+    init_wifi();
     // FreeRTOS多线程处理  Create a connection task with 8kB stack on core 0
     // xTaskCreatePinnedToCore(init_wifi_multi_thread, "WiFiTask", 8192, NULL, 3, NULL, 0);
 }
@@ -23,7 +23,7 @@ void loop() {
 // write your code here
     Serial.print("Hello World Embedded ESP32! \n");
     // 获取硬件信息
-    get_chip_info();
+    // get_chip_info();
     delay(1000);
     // 开发板LED 闪动的实现
     set_led();
