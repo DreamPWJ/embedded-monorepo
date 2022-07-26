f1ded0e51e237119d4a15c82b08aa53a1e9ca261
panweiji <406798106@qq.com>
2022-07-26 11:39:32 +0800

    feat: 无线Wifi和蓝牙模块功能

--- projects/esp32-demo/lib/wifi_network/wifi_network.cpp
+++ projects/esp32-demo/lib/wifi_network/wifi_network.cpp
@@ -1,7 +1,6 @@
 #include "wifi_network.h"
 #include <Arduino.h>
 #include <WiFi.h>
-#include <esp_wifi.h>
 
 //there's an include for this but it doesn't define the function if it doesn't think it needs it, so manually declare the function
 extern "C" void phy_bbpll_en_usb(bool en);
@@ -25,8 +24,9 @@ void init_wifi() {
     Serial.println("开始初始化WiFi模块...");
     // ESP32 WiFiMulti功能：连接到多个网络中的最强的 Wi-Fi 网络
     // 站模式：ESP32 连接到接入点连接到另一个网络, 它必须处于工作站模式
+    // WiFi.useStaticBuffers(true);
     WiFi.mode(WIFI_STA);
-    // 解决ESP32-C3 原生 USB CDC 在使用 Wifi 时停止工作
+    // 解决ESP32-C3 原生 USB CDC 在使用 WiFi 时停止工作
     phy_bbpll_en_usb(true); // this brings the USB serial-jtag back to life. Suggest doing this immediately after wifi startup.
     // 设置wifi账号和密码  注意文件名称不要和库的名称一样，会导致error: 'WiFi' was not declared in this scope
     WiFi.disconnect();
