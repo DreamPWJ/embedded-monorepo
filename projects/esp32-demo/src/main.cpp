11f553945ddee1dbce5c5474eb05c9b95a41f431
panweiji <406798106@qq.com>
2022-07-29 10:30:24 +0800

    feat: 集成阿里云IoT物联网平台

--- projects/esp32-demo/src/main.cpp
+++ projects/esp32-demo/src/main.cpp
@@ -21,10 +21,10 @@ void setup() {
     // FreeRTOS实时系统多线程处理  Create a connection task with 8kB stack on core 0
     // xTaskCreatePinnedToCore(init_wifi_multi_thread, "WiFiTask", 8192, NULL, 3, NULL, 0);
     // 网络请求
-    http_get("https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=18863302302");
-/*    http_post(
+    // http_get("https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=18863302302");
+    http_post(
             "https://oapi.dingtalk.com/robot/send?access_token=383391980b120c38f0f9a4a398349739fa67a623f9cfa834df9c5374e81b2081",
-            "");*/
+            "");
     // init_aliyun_iot_sdk();
     // }
 }
