8538d1c268803646e98a34447a9c4a8a79a6c688
panweiji <406798106@qq.com>
2022-08-03 14:42:13 +0800

    chore: 集成MQTT消息队列与OTA空中升级功能

--- projects/esp32-demo/lib/ota/ota.cpp
+++ projects/esp32-demo/lib/ota/ota.cpp
@@ -6,7 +6,7 @@
 * @date 2022/7/29 15:56
 * @description 固件OTA空中升级
 * 参考文档：https://blog.51cto.com/u_15284384/3054914
- * https://github.com/chrisjoyce911/esp32FOTA
+* https://github.com/chrisjoyce911/esp32FOTA
 */
 
 //esp32FOTA esp32FOTA("esp32-fota-http", "1.0.0");
