3384c52a34b2fc1baf6f1a903f46fc1414da89af
panweiji <406798106@qq.com>
2022-08-21 09:02:15 +0800

    eat: 集成PWM脉冲宽度调制

--- projects/esp32-demo/lib/pwm/pwm.h
+++ projects/esp32-demo/lib/pwm/pwm.h
@@ -1,6 +1,8 @@
 #ifndef ESP32_DEMO_MQTT_H
 #define ESP32_DEMO_MQTT_H
 
+#include <Arduino.h>
+
 /**
 * @author 潘维吉
 * @date 2022/8/20 15:51
@@ -8,5 +10,8 @@
 */
 
 void init_motor();
+
+void pwm_set_duty(uint16_t DutyA, uint16_t DutyB);
+void motor_control(int Cnt_L, int Cnt_R);
 
 #endif
