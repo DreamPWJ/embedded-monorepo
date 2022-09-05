10f6c15129bd53568009f560a0163e30cd6dbed4
panweiji <406798106@qq.com>
2022-09-05 16:58:45 +0800

    feat: 集成定制UART通用异步收发送器

--- packages/athena-common/nb_iot/nb_iot.cpp
+++ packages/athena-common/nb_iot/nb_iot.cpp
@@ -15,13 +15,19 @@
  * 初始化NB网络协议
  */
 void init_nb_iot() {
+    // NB相关引脚初始化
+    pinMode(6, OUTPUT);
+    pinMode(10, OUTPUT);
+    digitalWrite(6, HIGH);
+    digitalWrite(10, HIGH);
+
     // 给NB模组发送AT指令  NB模组出厂自带AT固件 接入天线
-    Serial.println("AT"); // 测试AT指令
-    Serial.println("AT+ECICCID\r\n"); // 查看SIM ID号
+    //Serial.println("AT"); // 测试AT指令
+    //Serial.println("AT+ECICCID\r\n"); // 查看SIM ID号
     Serial.println("AT+CGATT=1\r\n"); // 附着网络
     Serial.println("AT+CGDCONT=1,'IP','CMNET'\r\n"); // 注册APNID接入网络
     Serial.println("AT+CGACT=1\r\n"); // 激活网络
-    Serial.println("AT+ECPING='www.baidu.com'\r\n"); // 测试网络
+    //Serial.println("AT+ECPING='www.baidu.com'\r\n"); // 测试网络
 }
 
 
--- packages/athena-utils/uart/uart.cpp
+++ packages/athena-utils/uart/uart.cpp
@@ -26,7 +26,7 @@
 static void set_uart(void *pvParameters) {
     const uart_port_t uart_num = UART_NUM_1;
     uart_config_t uart_config = {
-            .baud_rate = 115200,
+            .baud_rate = 9600,
             .data_bits = UART_DATA_8_BITS,
             .parity = UART_PARITY_DISABLE,
             .stop_bits = UART_STOP_BITS_1,
