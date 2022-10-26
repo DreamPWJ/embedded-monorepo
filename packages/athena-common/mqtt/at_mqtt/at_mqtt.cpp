ce3afc92a5bc4965f70e103f660330740d62be72
panweiji <406798106@qq.com>
2022-10-26 09:57:10 +0800

    chore: 增强嵌入式代码的健壮性和可维护性

--- packages/athena-common/mqtt/at_mqtt/at_mqtt.cpp
+++ packages/athena-common/mqtt/at_mqtt/at_mqtt.cpp
@@ -205,7 +205,7 @@ void at_mqtt_callback(void *pvParameters) {
     String flag = "ECMTRECV"; // 并发情况下 串口可能返回多条数据
     String flagRSSI = "+CSQ:"; // 并发情况下 串口可能返回多条数据
     while (1) {  // RTOS多任务条件： 1. 不断循环 2. 无return关键字
-        // yield(); // 专用于主动调用运行后台。 在ESP单片机实际运行过程中，有时会不可避免需要长时间延时，这些长时间延时可能导致单线程的C/C++后台更新不及时，会导致看门狗触发 可使用yield()；主动调用后台程序防止重启。
+        yield(); // 专用于主动调用运行后台。 在ESP单片机实际运行过程中，有时会不可避免需要长时间延时，这些长时间延时可能导致单线程的C/C++后台更新不及时，会导致看门狗触发 可使用yield()；主动调用后台程序防止重启。
         // delay(10);
         if (myMqttSerial.available() > 0) { // 串口缓冲区有数据 数据长度
             /*
@@ -217,7 +217,7 @@ void at_mqtt_callback(void *pvParameters) {
 #if true
             Serial.println("------------------------------------");
             Serial.println(incomingByte);
-           // Serial.println("************************************");
+            Serial.println("************************************");
 #endif
 
             if (incomingByte.indexOf(flag) != -1) {
