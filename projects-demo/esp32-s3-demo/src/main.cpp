f6414de38eb595e493c4566cb0a0c23a95227475
panweiji <406798106@qq.com>
2023-02-06 11:02:11 +0800

    feat: 全新主控单片机和三轴磁阻传感器

--- projects-demo/esp32-s3-demo/src/main.cpp
+++ projects-demo/esp32-s3-demo/src/main.cpp
@@ -2,9 +2,13 @@
 
 void setup() {
 // write your initialization code here
-    Serial.println("测试新主控芯片");
+    // 设置UART串口波特率
+    Serial.begin(115200);
+    Serial.println("你好, ESP32 S3单片机");
 }
 
 void loop() {
 // write your code here
+    delay(1000);
+    Serial.println("测试新主控芯片循环执行");
 }
