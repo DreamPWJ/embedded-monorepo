1a781cd25ab9171fd92deb8bcd1312a0d4f7dce9
panweiji <406798106@qq.com>
2023-02-14 11:38:24 +0800

    feat: 全新主控单片机和三轴磁阻传感器

--- packages/athena-common/ota/ota.cpp
+++ packages/athena-common/ota/ota.cpp
@@ -93,7 +93,7 @@ void do_firmware_upgrade(String version, String jsonUrl, String firmwareUrl) {
     if ((new_version != "null" && version_compare(new_version, version) == 1) ||
         (new_version.indexOf("NEW_VERSION") != -1)) {
         // 做固件MD5签名算法 保证固件本身是安全的  升级json文件中的原始md5值和http请求头Content-MD5中的md5值保持一致
-        printf("当前固件版本v%s, 有新v%s版本OTA固件, 正在下载... \n", version.c_str(), new_version.c_str());
+        printf("当前固件版本v%s, 有新OTA固件v%s版本, 正在下载... \n", version.c_str(), new_version.c_str());
 #if WIFI_ONLY_OTA
         // 上报MQTT消息
         string jsonDataDo = "{\"msg\":\"发现新版本了, 开始执行OTA空中升级\",\"chipId\":\"" + to_string(chipId) + "\"}";
