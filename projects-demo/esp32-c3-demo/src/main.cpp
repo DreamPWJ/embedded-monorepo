efac7eeaee911ffe5c5eb0cc1e2b09f97d2bd4e7
panweiji <406798106@qq.com>
2023-03-20 17:07:00 +0800

    chore: 增强嵌入式代码的健壮性和可维护性

--- projects-demo/esp32-c3-demo/src/main.cpp
+++ projects-demo/esp32-c3-demo/src/main.cpp
@@ -1,6 +1,20 @@
 #include <Arduino.h>
+
+#define XSTR(x) #x
+#define STR(x) XSTR(x)
+
 void setup() {
 // write your initialization code here
+    // 初始化设置代码  为保证单片机运行正常  电路设计与电压必须稳定
+
+    // 设置UART串口波特率
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("ESP32 C3 MCU");
+    const char* mqtt_version = STR(MQTT_VERSION);
+    Serial.println(mqtt_version);
+    const char* app_version = STR(APP_VERSION);
+    Serial.println(app_version);
 }
 
 void loop() {
