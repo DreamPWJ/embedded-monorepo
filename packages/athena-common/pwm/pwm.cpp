ea306c80e3c8c8c3de374333dc69b4f0e6003634
panweiji <406798106@qq.com>
2023-07-06 10:14:02 +0800

    chore: 增强嵌入式代码的健壮性和可维护性

--- packages/athena-common/pwm/pwm.cpp
+++ packages/athena-common/pwm/pwm.cpp
@@ -248,10 +248,10 @@ int get_pwm_status() {
         ledcWrite(channel_PWMB, 0);
         //Serial.println("电机下限位状态触发");
         return 0;
-    } else if (upper_limit == 1 && lower_limit == 0) {
+    } else if (upper_limit == 0 && lower_limit == 1) {
         //Serial.println("电机运行状态触发");
         return 2;
-    } else if (upper_limit == 0 && lower_limit == 1) {
+    } else if (upper_limit == 1 && lower_limit == 0) {
         ledcWrite(channel_PWMA, 0);
         ledcWrite(channel_PWMB, 0);
         //Serial.println("电机无效状态触发");
