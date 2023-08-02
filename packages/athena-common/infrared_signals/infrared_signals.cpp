629c87a10820ae20b7670c7af3fa872bc8a0bc60
panweiji <406798106@qq.com>
2023-08-02 09:07:04 +0800

    chore: 增强嵌入式代码的健壮性和可维护性

--- packages/athena-common/infrared_signals/infrared_signals.cpp
+++ packages/athena-common/infrared_signals/infrared_signals.cpp
@@ -1,6 +1,6 @@
 #include "infrared_signals.h"
 #include <Arduino.h>
-#include <IRremote.h>
+#include <IRremote.hpp>
 
 /**
 * @author 潘维吉
@@ -8,23 +8,23 @@
 * @description 红外信号发送接收数据
 */
 
-IRrecv irrecv(RECV_PIN);
-
-decode_results results; // 注意这个数据类型
 
 /**
  * 初始化
  */
 void init(void) {
-    irrecv.enableIRIn(); // 初始化红外接收器
+    IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK); // Start the receiver // 初始化红外接收器
 }
 
 /**
  * 获取红外数据
  */
 void get_data(void) {
-    if (irrecv.decode(&results)) {  // 注意这里取解码结果的方法，传递的是变量指针。
-        Serial.println(results.value, HEX); // 以16进制换行输出接收代码。注意取值方法【results.value】。
-        irrecv.resume(); // 给红外传送指令，让其继续接收下一个值。
+    if (IrReceiver.decode()) {  // 注意这里取解码结果的方法，传递的是变量指针。
+        Serial.println(IrReceiver.decodedIRData.decodedRawData,
+                       HEX); // Print raw data  以16进制换行输出接收代码
+        IrReceiver.printIRResultShort(&Serial); // Print complete received data in one line
+        IrReceiver.printIRSendUsage(&Serial);   // Print the statement required to send this data
+        IrReceiver.resume(); // 给红外传送指令，让其继续接收下一个值。
     }
 }
